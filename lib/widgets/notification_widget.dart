76d4ccb79f8a29c75403be3a06b75d0fc1d5b1c4
AE-GDY <74915033+AE-GDY@users.noreply.github.com>
2022-05-08 16:55:06 +0200

    Add files via upload

---
+++ lib/widgets/notification_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class NotificationWidget{
+  String notificationSender;
+  String mainText;
+  String notificationType;
+
+  NotificationWidget({
+    required this.notificationSender,
+    required this.mainText,
+    required this.notificationType,
+  });
+
+}
